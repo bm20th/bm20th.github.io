ad3e3348d531d388280f4855d94a9c182e7cb071
k-two <32743819+ktwkubota@users.noreply.github.com>
2017-12-24 03:45:32 +0900

    クリスマスだよ五鍵大会Web更新
    TOPを変更。
    当たり障りのなさそうな写真をスライド形式で12枚表示するように変えました。
    あと「参加ありがとう」メッセージを追加

--- js/td5.js
+++ js/td5.js
@@ -21,4 +21,13 @@ $(function(){
 		}
 	);
 
+	jQuery(function($) {
+	    $('.bg-slider').bgSwitcher({
+	        images: ['./images/slide01.jpg','./images/slide02.jpg','./images/slide03.jpg','./images/slide04.jpg','./images/slide05.jpg','./images/slide06.jpg','./images/slide07.jpg','./images/slide08.jpg','./images/slide09.jpg','./images/slide10.jpg','./images/slide11.jpg','./images/slide12.jpg'], // 切り替える背景画像を指定
+					interval: 4000, // 背景画像を切り替える間隔を指定 3000=3秒
+          loop: true, // 切り替えを繰り返すか指定 true=繰り返す　false=繰り返さない
+				  shuffle: true, // 背景画像の順番をシャッフルするか指定 true=する　false=しない
+	    });
+	});
+
 });
